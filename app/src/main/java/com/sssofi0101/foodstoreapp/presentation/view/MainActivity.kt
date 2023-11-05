196148866789924c83a95d4c5ba937d484faf785
sssofi0101 <55346176+sssofi0101@users.noreply.github.com>
2023-11-05 14:13:51 +0300

    Добавлено кеширование выбранного города
    Реализовано с помощью SharedPreferences

--- app/src/main/java/com/sssofi0101/foodstoreapp/presentation/view/MainActivity.kt
+++ app/src/main/java/com/sssofi0101/foodstoreapp/presentation/view/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.sssofi0101.foodstoreapp.presentation.view
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.SpinnerAdapter
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -25,7 +30,7 @@ import com.sssofi0101.foodstoreapp.presentation.viewmodel.MenuViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +39,31 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar: Toolbar = binding.mainToolbar
 
-        setSupportActionBar(binding.mainToolbar)
+        setSupportActionBar(toolbar)
         val rootView = window.decorView as ViewGroup
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.customView = layoutInflater.inflate(R.layout.action_bar, rootView, false)
+
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+        val lastLocation = sharedPref.getInt("lastLocation", 1)
+        val citySpinner = toolbar.findViewById<Spinner>(R.id.citySpinner)
+        citySpinner.setSelection(lastLocation)
+        citySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                sharedPref.edit().putInt("lastLocation",position).apply()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+
 
         val navView: BottomNavigationView = binding.bottomNavigationView
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
